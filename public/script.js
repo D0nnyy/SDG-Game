ef671cbba7c6d1bbe601ac5acf1d5568aced10ba
X <129168580+dwffssfsfwpefjsifiosfknlfsemnfrklefios@users.noreply.github.com>
2023-11-01 12:19:42 +0100

    excellent

--- public/script.js
+++ public/script.js
@@ -1,6 +1,44 @@
 let sdgs = document.getElementsByClassName("sdg");
 let shuffledSdgs = Array.from(sdgs).sort(() => Math.random() - 0.5);
+let toggle = false;
+let backBtn = document.getElementById("backbtn");
 
 for (let i = 0; i < 3; i++) {
-  shuffledSdgs[i].style.display = "flex";
+  const sdg = shuffledSdgs[i];
+  const sdgNum = sdg.id.replace("sdg", "");
+
+  const modal = document.getElementById(`modal-sdg${sdgNum}`);
+
+  backBtn = function() {
+    modal.style.display = "none";
+
+    console.log("a")
+
+    for (let i = 0; i < sdgs.length; i++) {
+      sdgs[i].style.display = "flex";
+    }
+
+    toggle = false
+  }
+
+  sdg.onclick = function() {
+    if (toggle === false) {
+      modal.style.display = "flex";
+
+      for (let i = 0; i < sdgs.length; i++) {
+        sdgs[i].style.display = "none";
+      }
+
+    } else {
+      modal.style.display = "none";
+
+      for (let i = 0; i < sdgs.length; i++) {
+        sdgs[i].style.display = "flex";
+      }
+    }
+
+    toggle = !toggle
+  }
+
+  sdg.style.display = "flex";
 }
--- source/views/sdg.php
+++ source/views/sdg.php
@@ -7,7 +7,10 @@
     <button type="button" id="backbtn">
         Back!
     </button>
-
+    
+    <div>
+        <img class="sdg-img" src="<?=$row['img']?>"></img>
+    </div>
     <h2><?=$row["title"]?></h2>
     <p><?=$row["about"]?><p>
 </div>
